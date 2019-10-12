5f131de1bfc075c28069f556dd7cb9ddb1c52959
user user <user@workspacewqpacq1r0zaxkm13.go-cli-777869fcf6-zs6w4>
2019-10-12 18:37:34 +0000

    adds basic webrouting

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module faktura
 
 go 1.12
+
+require (
+	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
+	github.com/goarne/config v0.0.0-20170728211351-a45a4fb6af75
+	github.com/goarne/logging v0.0.0-20170730213320-726f1796d37e
+)
--- main.go
+++ main.go
@@ -1,6 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/goarne/config"
+	"github.com/goarne/logging"
+)
+
+var (
+	appConfig       AppConfig
+	msortWebService Service
+)
+
+type AppConfig struct {
+	ServerPort  int32 `yaml:"serviceurl"`
+	Infologger  logging.LogConfig
+	ErrorLogger logging.LogConfig
+}
+
+func init() {
+	appConfig = AppConfig{}
+
+	cl := config.ConfigLoader{"config.d/appconfig.yaml"}
+
+	//Adding the concrete unmarshalling of AppConfig structure to the generic Config function.
+
+	cl.LoadAppKonfig(&appConfig)
+
+	rotatingTraceWriter := logging.CreateRotatingWriter(appConfig.Infologger)
+	rotatingErrorWriter := logging.CreateRotatingWriter(appConfig.ErrorLogger)
+
+	tracerLogger := logging.CreateLogWriter(rotatingTraceWriter)
+	tracerLogger.Append(os.Stdout)
+
+	errorLogger := logging.CreateLogWriter(rotatingErrorWriter)
+	errorLogger.Append(os.Stdout)
+
+	logging.InitLoggers(tracerLogger, tracerLogger, errorLogger, errorLogger)
+}
 
 func main() {
 	HelloWorld()
